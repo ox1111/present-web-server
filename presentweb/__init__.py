aca4a37c853111682f168814aa60cd2c5ad8c776
wisemuji <wisemuji@gmail.com>
2019-06-05 08:38:23 +0900

    Sending mail with Flask-Mail

--- presentweb/__init__.py
+++ presentweb/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'enter-your-mail',
+	MAIL_PASSWORD = 'enter-your-password^'
+	)
+mail = Mail(app)
+
 
 @app.route("/")
-def presentweb():
-    return "Server running"
+def helloworld():
+    return "Hello World"
+
+@app.route('/send-mail/')
+def send_mail():
+	try:
+		msg = Message("Send Mail Tutorial!",
+		  sender="enter-your-mail",
+		  recipients=["where-to-send"])
+		msg.body = "Sending mail test"           
+		mail.send(msg)
+		return 'Mail sent'
+	except Exception as e:
+		return(str(e)) 
